23aaa0c288688794d53fc793c753d70874c90768
Kaisei Sunaga <jkdwhssk@yahoo.co.jp>
2019-08-04 00:27:43 +0900

    Initial Commit

---
+++ Dereport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dereport
+//
+//  Created by 砂賀開晴 on 2019/08/04.
+//  Copyright © 2019 Kaisei Sunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
